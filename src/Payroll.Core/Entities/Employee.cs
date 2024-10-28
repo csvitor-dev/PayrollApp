7e4fbe58a295ded55eb02cb5df6340cedd53d2ff
csvitor-dev <csvitor.dev@gmail.com>
2024-10-28 08:40:06 -0300

    feat(core): expanding the 'Employee' entity with contracts

--- src/Payroll.Core/Entities/Employee.cs
+++ src/Payroll.Core/Entities/Employee.cs
@@ -1,6 +1,20 @@
+using Payroll.Core.Contracts;
+
 namespace Payroll.Core.Entities;
 
 public class Employee
 {
+    public int ID { get; }
     public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public IPaymentClassification Classification { get; set; } = null!;
+    public IPaymentSchedule Schedule { get; set; } = null!;
+    public IPaymentMethod Method { get; set; } = null!;
+   
+    public Employee(int id, string name, string address)
+    {
+        ID = id;
+        Name = name;
+        Address = address;
+    }
 }
