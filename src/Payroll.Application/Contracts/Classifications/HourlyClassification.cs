e5cabbb21c3f99ab5747ce44aff1f7a0f0c919bd
csvitor-dev <csvitor.dev@gmail.com>
2024-11-05 08:48:32 -0300

    feat: creating the 'TimeCardTransaction' class in detail, test passed

--- src/Payroll.Application/Contracts/Classifications/HourlyClassification.cs
+++ src/Payroll.Application/Contracts/Classifications/HourlyClassification.cs
@@ -1,9 +1,15 @@
 using Payroll.Core.Contracts;
+using Payroll.Core.Entities;
 
 namespace Payroll.Application.Contracts.Classifications;
 
 public class HourlyClassification(double hourlyRate) : IPaymentClassification
 {
     public double HourlyRate { get; set; } = hourlyRate;
-    public IList<string> TimeCards { get; } = [];
+    public IList<TimeCard> TimeCards { get; } = [];
+
+    public void AddTimeCard(TimeCard timeCard)
+        => TimeCards.Add(timeCard);
+    public TimeCard? GetTimeCard(DateTime dateTime)
+        => TimeCards.FirstOrDefault(t => t.Date == dateTime);
 }
---
+++ src/Payroll.Application/Transactions/Assets/TimeCardTransaction.cs
@@ -0,0 +1,18 @@
+using Payroll.Application.Contracts.Classifications;
+using Payroll.Core.Data;
+
+namespace Payroll.Application.Transactions.Assets;
+
+public class TimeCardTransaction(int id, DateTime date, double hours) : ITransaction
+{
+    public void Execute()
+    {
+        var e = PayrollDB.GetEmployee(id);
+
+        if (e is null) throw new InvalidOperationException("No such employee");
+
+        if (e.Classification is not HourlyClassification hc) 
+            throw new InvalidOperationException("No hourly classification");
+        hc.AddTimeCard(new(date, hours));
+    }
+}
