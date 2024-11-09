7d90ad160093c57b75cd113234629b76cda7ce14
csvitor-dev <csvitor.dev@gmail.com>
2024-11-09 11:49:53 -0300

    test: creating test case for 'ChangeAddressTransaction', test failed

--- test/Payroll.Test/ChangeEmployeeTransactionTest.cs
+++ test/Payroll.Test/ChangeEmployeeTransactionTest.cs
@@ -1,4 +1,5 @@
 using Payroll.Application.Transactions.Add;
+using Payroll.Application.Transactions.Change;
 using Payroll.Core.Data;
 using Payroll.Core.Entities;
 
@@ -11,14 +12,31 @@ public class ChangeEmployeeTransactionTest
     public void Test_ChangeNameTransaction()
     {
         int empId = 10;
+        string newName = "Bob";
         AddHourlyEmployee t = new(empId, "Bill", "Home", 15.25);
-        ChangeNameTransaction cnt = new(empId, "Bob");
+        ChangeNameTransaction cnt = new(empId, newName);
 
         t.Execute();
         cnt.Execute();
         Employee? e = PayrollDb.GetEmployee(empId);
         
         Assert.That(e, Is.Not.Null);
-        Assert.That(e.Name, Is.EqualTo("Bob"));
+        Assert.That(e.Name, Is.EqualTo(newName));
+    }
+
+    [Test]
+    public void Test_ChangeAddressTransaction()
+    {
+        int empId = 11;
+        string newAddress = "St. Rowling";
+        AddHourlyEmployee t = new(empId, "Chris", "Home", 12.95);
+        ChangeAddressTransaction cdt = new(empId, newAddress);
+
+        t.Execute();
+        cdt.Execute();
+        Employee? e = PayrollDb.GetEmployee(empId);
+        
+        Assert.That(e, Is.Not.Null);
+        Assert.That(e.Address, Is.EqualTo(newAddress));
     }
 }
