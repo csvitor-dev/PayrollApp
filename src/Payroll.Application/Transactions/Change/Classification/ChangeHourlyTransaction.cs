05723f1a82d22b59cad3b63f874ce26e45b371db
csvitor-dev <csvitor.dev@gmail.com>
2024-11-12 08:42:15 -0300

    refactor: organizing code format

--- src/Payroll.Application/Transactions/Change/Classification/ChangeHourlyTransaction.cs
+++ src/Payroll.Application/Transactions/Change/Classification/ChangeHourlyTransaction.cs
@@ -6,6 +6,8 @@ namespace Payroll.Application.Transactions.Change.Classification;
 
 public class ChangeHourlyTransaction(int id, double hourlyRate) : ChangeClassificationTransaction(id)
 {
-    protected override IPaymentClassification Classification => new HourlyClassification(hourlyRate);
-    protected override IPaymentSchedule Schedule => new WeeklySchedule();
+    protected override IPaymentClassification Classification 
+        => new HourlyClassification(hourlyRate);
+    protected override IPaymentSchedule Schedule 
+        => new WeeklySchedule();
 }
