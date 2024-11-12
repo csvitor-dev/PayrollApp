2b03bb108ae13ccb06429dbe7f5c2d797cf758ea
csvitor-dev <csvitor.dev@gmail.com>
2024-11-12 08:32:29 -0300

    test: creating test case for 'ChangeMethodTransaction' -> 'ChangeDirectTransaction', test failed

---
+++ test/Payroll.Test/Change/ChangeMethodTransactionTest.cs
@@ -0,0 +1,39 @@
+using Payroll.Application.Transactions.Add;
+using Payroll.Core.Contracts;
+using Payroll.Core.Data;
+using Payroll.Core.Entities;
+
+namespace Payroll.Test.Change;
+
+[TestFixture]
+public class ChangeMethodTransactionTest
+{
+    [Test]
+    public void Test_ChangeDirectTransaction()
+    {
+        int id = 15;
+        AddSalariedEmployee t = new(id, "John", "St. Louis", 2100.00);
+        string bank = "021000322"; // ABA Routing Number - Bank of America
+        string account = "298309-00";
+        ChangeDirectTransaction cdt = new(id, bank, account);
+        
+        t.Execute();
+        cdt.Execute();
+        Employee? e = PayrollDb.GetEmployee(id);
+        IPaymentMethod? pm = e?.Method;
+        DirectMethod? dm = pm as DirectMethod;
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(e, Is.Not.Null);
+            Assert.That(pm, Is.Not.Null);
+            Assert.That(dm, Is.Not.Null);
+        });
+        Assert.Multiple(() =>
+        {
+            Assert.That(pm is DirectMethod, Is.True);
+            Assert.That(dm.Bank, Is.EqualTo(bank));
+            Assert.That(dm.Account, Is.EqualTo(account));
+        });
+    }
+}
