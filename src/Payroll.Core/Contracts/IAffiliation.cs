21249a4677200d57fa84853ae9dc39e77be8ad5b
csvitor-dev <csvitor.dev@gmail.com>
2024-11-19 13:04:24 -0300

    refactor: segregating responsabilities of 'IAffiliation' interface

--- src/Payroll.Core/Contracts/IAffiliation.cs
+++ src/Payroll.Core/Contracts/IAffiliation.cs
@@ -2,8 +2,4 @@ using Payroll.Core.Entities;
 
 namespace Payroll.Core.Contracts;
 
-public interface IAffiliation
-{
-    public void AddServiceCharge(ServiceCharge charge);
-    public ServiceCharge? GetServiceCharge(DateTime date);
-}
+public interface IAffiliation;
