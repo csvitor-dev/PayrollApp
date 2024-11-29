0270ee5e53be504737a0e807332e1cdfef240efa
csvitor-dev <csvitor.dev@gmail.com>
2024-11-29 09:04:43 -0300

    fix: by default, 'NoAffiliation' is assigned to an 'Employee'
    <DETAILS> it turns out that, as every employee initially has no
    affiliation, it is necessary to pass this instance to avoid null
    references

--- src/Payroll.Application/Transactions/Add/AddEmployeeTransaction.cs
+++ src/Payroll.Application/Transactions/Add/AddEmployeeTransaction.cs
@@ -1,3 +1,4 @@
+using Payroll.Application.Contracts.Affiliations;
 using Payroll.Core.Contracts;
 using Payroll.Core.Data;
 using Payroll.Core.Entities;
@@ -5,7 +6,7 @@ using Payroll.Application.Contracts.Classifications;
 
 namespace Payroll.Application.Transactions.Add;
 
-public abstract class AddEmployeeTransaction(int empID, string name, string address) 
+public abstract class AddEmployeeTransaction(int empID, string name, string address)
     : ITransaction
 {
     protected abstract IPaymentClassification MakeClassification(); // FACTORY METHOD
@@ -13,14 +14,18 @@ public abstract class AddEmployeeTransaction(int empID, string name, string addr
 
     public void Execute()
     {
-        IPaymentClassification pc = MakeClassification();
-        IPaymentSchedule ps = MakeSchedule();
+        var pc = MakeClassification();
+        var ps = MakeSchedule();
         IPaymentMethod pm = new HoldMethod();
+        IAffiliation aff = new NoAffiliation();
 
-        Employee e = new(empID, name, address);
-        e.Classification = pc;
-        e.Schedule = ps;
-        e.Method = pm;
+        Employee e = new(empID, name, address)
+        {
+            Affiliation = aff,
+            Classification = pc,
+            Schedule = ps,
+            Method = pm
+        };
 
         PayrollDb.AddEmployee(empID, e);
     }
