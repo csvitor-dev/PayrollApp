9d7db5a5ac7845c27734eebe30402414486d5433
csvitor-dev <csvitor.dev@gmail.com>
2024-12-01 08:56:25 -0300

    test('PayHourlyEmployee'): create a test case when 'HourlyEmployee' has time cards, test failed

--- test/Payroll.Test/Payday/PayHourlyEmployeeTest.cs
+++ test/Payroll.Test/Payday/PayHourlyEmployeeTest.cs
@@ -1,4 +1,5 @@
 using Payroll.Application.Transactions.Add;
+using Payroll.Application.Transactions.Assets;
 using Payroll.Application.Transactions.Payday;
 using Payroll.Core.Entities;
 
@@ -14,11 +15,28 @@ public class PayHourlyEmployeeTest
         AddHourlyEmployee t = new(id, "Bill", "Home", 15.25);
         DateTime payDate = new(2001, 11, 9);
         PaydayTransaction pt = new(payDate);
-        
+
         t.Execute();
         pt.Execute();
         Paycheck? pc = pt.GetPaycheck(id);
-        
+
         PaycheckValidator.Validate(pc, payDate, 0.0);
+    }
+
+    [Test]
+    public void Test_PaySingleHourlyEmployee_OneTimeCard()
+    {
+        int id = 23;
+        AddHourlyEmployee t = new(id, "Johnson", "Home", 15.25);
+        DateTime payDate = new(2001, 11, 9);
+        TimeCardTransaction tc = new(id, payDate, 2.0);
+        PaydayTransaction pt = new(payDate);
+
+        t.Execute();
+        tc.Execute();
+        pt.Execute();
+        var pc = pt.GetPaycheck(id);
+
+        PaycheckValidator.Validate(pc, payDate, 2.0 * 15.25);
     }
 }
