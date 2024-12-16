4de45e73f7e9ca78462926ff081420a7bef38d0e
csvitor-dev <csvitor.dev@gmail.com>
2024-12-16 13:05:17 -0300

    test: create a test case for 'CommissionedEmployee' when has one sales receipt, test failed

--- test/Payroll.Test/Payday/PayCommissionedEmployeeTest.cs
+++ test/Payroll.Test/Payday/PayCommissionedEmployeeTest.cs
@@ -1,4 +1,5 @@
 using Payroll.Application.Transactions.Add;
+using Payroll.Application.Transactions.Assets;
 using Payroll.Application.Transactions.Payday;
 
 namespace Payroll.Test.Payday;
@@ -19,5 +20,22 @@ public class PayCommissionedEmployeeTest
         var pc = pt.GetPaycheck(id);
         
         PaycheckValidator.Validate(pc, payDate, 1000.0);
+    }
+
+    [Test]
+    public void Test_PaySingleCommissionedEmployee_OneSalesReceipts()
+    {
+        int id = 29;
+        AddCommissionedEmployee t = new(id, "Wilson", "Home", 1575.0, 1.2);
+        DateTime payDate = new(2001, 11, 9);
+        SalesReceiptTransaction srt = new(id, payDate, 250.5);
+        PaydayTransaction pt = new(payDate);
+        
+        t.Execute();
+        srt.Execute();
+        pt.Execute();
+        var pc = pt.GetPaycheck(id);
+        
+        PaycheckValidator.Validate(pc, payDate, 1575.0 + 1.2 * 250.5);
     }
 }
