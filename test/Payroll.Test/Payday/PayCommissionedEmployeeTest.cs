eb1b670e5f163f9f4ff38803afd9f8e096ea393b
csvitor-dev <csvitor.dev@gmail.com>
2024-12-19 08:20:43 -0300

    test: create a test case for 'CommissionedEmployee' when has two payment periods, test failed

--- test/Payroll.Test/Payday/PayCommissionedEmployeeTest.cs
+++ test/Payroll.Test/Payday/PayCommissionedEmployeeTest.cs
@@ -74,4 +74,24 @@ public class PayCommissionedEmployeeTest
 
         PaycheckValidator.Validate(pc, payDate, 1750.0 + 2.1 * (150.0 + 200.25));
     }
+
+    [Test]
+    public void Test_PaySingleCommissionedEmployee_OnTwoPayPeriods()
+    {
+        int id = 32;
+        AddCommissionedEmployee t = new(id, "Victor", "Home", 1200.0, 2.0);
+        DateTime previousPeriod = new(2001, 11, 9);
+        DateTime currentPeriod = new(2001, 11, 23);
+        SalesReceiptTransaction srt1 = new(id, previousPeriod, 150.0);
+        SalesReceiptTransaction srt2 = new(id, currentPeriod, 520.5);
+        PaydayTransaction pt = new(currentPeriod);
+
+        t.Execute();
+        srt1.Execute();
+        srt2.Execute();
+        pt.Execute();
+        var pc = pt.GetPaycheck(id);
+
+        PaycheckValidator.Validate(pc, currentPeriod, 1200.0 + 2.0 * 520.5);
+    }
 }
