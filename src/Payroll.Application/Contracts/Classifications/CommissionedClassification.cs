496083eedcbcf231200d7046262015c3828ce918
csvitor-dev <csvitor.dev@gmail.com>
2024-12-19 08:40:14 -0300

    feat: improve 'CommissionedClassification' for calculate pay in correct period, test passed

--- src/Payroll.Application/Contracts/Classifications/CommissionedClassification.cs
+++ src/Payroll.Application/Contracts/Classifications/CommissionedClassification.cs
@@ -8,15 +8,28 @@ public class CommissionedClassification(double salary, double commissionRate) : 
     public double Salary { get; set; } = salary;
     public double CommissionRate { get; set; } = commissionRate;
     public IList<SalesReceipt> SalesReceipts { get; } = [];
-    
+
     public void AddSalesReceipt(SalesReceipt sales)
         => SalesReceipts.Add(sales);
+
     public SalesReceipt? GetSalesReceipt(DateTime date)
         => SalesReceipts.FirstOrDefault(s => s.Date == date);
 
     public double CalculatePay(Paycheck paycheck)
-        => Salary + CalculateCommission();
+        => Salary + CalculateCommission(paycheck.PayDate);
 
-    private double CalculateCommission() 
-        => SalesReceipts.Sum(s => s.Amount * CommissionRate);
+    private double CalculateCommission(DateTime payDate)
+    {
+        var salesReceiptInPeriod = from sales in SalesReceipts
+            where IsCurrentPaymentPeriod(payDate, sales.Date)
+            select sales;
+        
+        return salesReceiptInPeriod.Sum(CalculatePayForSalesReceipt);
+    }
+
+    private static bool IsCurrentPaymentPeriod(DateTime payDate, DateTime date)
+        => date > payDate.AddDays(-12) && date <= payDate;
+    
+    private double CalculatePayForSalesReceipt(SalesReceipt sales)
+        => sales.Amount * CommissionRate;
 }
