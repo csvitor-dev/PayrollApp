a4384b55e2b4e5d8bad07f952441c676d6e69025
csvitor-dev <csvitor.dev@gmail.com>
2024-12-20 09:05:57 -0300

    refactor: overload 'Validate()' method considering 'deductions' as parameter

--- test/Payroll.Test/Payday/PaycheckValidator.cs
+++ test/Payroll.Test/Payday/PaycheckValidator.cs
@@ -4,7 +4,10 @@ namespace Payroll.Test.Payday;
 
 public static class PaycheckValidator
 {
-    public static void Validate(Paycheck? paycheck, DateTime payDate, double pay)
+    public static void Validate(Paycheck? paycheck, DateTime payDate, double pay) 
+        => Validate(paycheck, payDate, 0.0, pay);
+
+    public static void Validate(Paycheck? paycheck, DateTime payDate, double deductions, double pay)
     {
         Assert.That(paycheck, Is.Not.Null);
         Assert.Multiple(() =>
@@ -12,7 +15,7 @@ public static class PaycheckValidator
             Assert.That(paycheck.PayDate, Is.EqualTo(payDate));
             Assert.That(paycheck.GrossPay, Is.EqualTo(pay));
             Assert.That(paycheck.GetField("Disposition"), Is.EqualTo("Hold"));
-            Assert.That(paycheck.Deductions, Is.EqualTo(0.0));
+            Assert.That(paycheck.Deductions, Is.EqualTo(deductions));
             Assert.That(paycheck.NetPay, Is.EqualTo(pay));
         });
     }
