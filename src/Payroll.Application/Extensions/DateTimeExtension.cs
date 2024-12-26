b5b582a748a2bcfc7dbd9abf9c7f606dacca8d93
csvitor-dev <csvitor.dev@gmail.com>
2024-12-26 10:11:21 -0300

    feat: create 'IsInPayPeriod' extension method for both 'Classification' and 'Affiliation' contracts to use
    <DETAILS> as both 'PaymentClassification' and 'Affiliation' need to
    determine the valid payment period, then they need to share this
    resource through a utility

--- src/Payroll.Application/Extensions/DateTimeExtension.cs
+++ src/Payroll.Application/Extensions/DateTimeExtension.cs
@@ -1,12 +1,17 @@
+using Payroll.Core.Entities;
+
 namespace Payroll.Application.Extensions;
 
 public static class DateTimeExtension
 {
-    public static int WeekOfMonth(this DateTime date)
+    public static int WeekOfMonth(this DateTime self)
     {
-        var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+        var firstDayOfMonth = new DateTime(self.Year, self.Month, 1);
         var dayOfWeek = (int)firstDayOfMonth.DayOfWeek;
 
-        return (date.Day + dayOfWeek - 1) / 7 % 4;
+        return (self.Day + dayOfWeek - 1) / 7 % 4;
     }
+
+    public static bool IsInPayPeriod(this DateTime self, Paycheck paycheck)
+        => self >= paycheck.StartDate && self <= paycheck.PayDate;
 }
