6be8612b43597f3cbc65551ba7ab6e9eee4a8df8
csvitor-dev <csvitor.dev@gmail.com>
2024-12-26 09:34:05 -0300

    test: create a test case for 'HourlyUnionMember' when has a service charge, test failed

--- test/Payroll.Test/Affiliation/UnionMemberDuesTest.cs
+++ test/Payroll.Test/Affiliation/UnionMemberDuesTest.cs
@@ -1,6 +1,8 @@
 using Payroll.Application.Transactions.Add;
+using Payroll.Application.Transactions.Assets;
 using Payroll.Application.Transactions.Change.Affiliation;
 using Payroll.Application.Transactions.Payday;
+using Payroll.Core.Entities;
 using Payroll.Test.Payday;
 
 namespace Payroll.Test.Affiliation;
@@ -24,5 +26,27 @@ public class UnionMemberDuesTest
         var pc = pt.GetPaycheck(empId);
         
         PaycheckValidator.Validate(pc, payDate, 5 * 9.42, 1522.59 - 5 * 9.42);
+    }
+
+    [Test]
+    public void HourlyUnionMemberServiceCharge()
+    {
+        int empId = 34;
+        int memId = 7735;
+        DateTime payDate = new(2001, 11, 9);
+        AddHourlyEmployee t = new(empId, "David", "Home", 15.24);
+        TimeCardTransaction tct = new(empId, payDate, 8.0);
+        ChangeMemberTransaction cmt = new(empId, memId, 9.42);
+        ServiceChargeTransaction sct = new(memId, payDate, 19.42);
+        PaydayTransaction pt = new(payDate);
+        
+        t.Execute();
+        tct.Execute();
+        cmt.Execute();
+        sct.Execute();
+        pt.Execute();
+        var pc = pt.GetPaycheck(empId);
+        
+        PaycheckValidator.Validate(pc, payDate, 9.42 + 19.42, 8 * 15.24 - (9.42 + 19.42));
     }
 }
