f786ca73b2778e9b07e69efc2ea40a23bf7bf9f0
csvitor-dev <csvitor.dev@gmail.com>
2025-01-01 12:31:20 -0300

    refactor: improves test case structure for delete a employee, test passed

--- src/Payroll.Core/Data/PayrollDB.cs
+++ src/Payroll.Core/Data/PayrollDB.cs
@@ -4,33 +4,33 @@ namespace Payroll.Core.Data;
 
 public static class PayrollDb
 {
-    private static readonly Dictionary<int, Employee> s_employees = [];
-    private static readonly Dictionary<int, Employee> s_affiliations = [];
+    private static readonly Dictionary<int, Employee> s_Employees = [];
+    private static readonly Dictionary<int, Employee> s_Affiliations = [];
 
     // Employee Table
     public static void AddEmployee(int id, Employee employee)
-        => s_employees.Add(id, employee);
+        => s_Employees.Add(id, employee);
 
     public static Employee? GetEmployee(int empId)
-        => s_employees.GetValueOrDefault(empId);
+        => s_Employees.GetValueOrDefault(empId);
     
     public static IList<Employee> GetEmployees()
-        => s_employees.Values.ToList();
+        => s_Employees.Values.ToList();
 
     public static void DeleteEmployee(int empId)
-        => s_employees.Remove(empId);
+        => s_Employees.Remove(empId);
 
     // Affiliation Table
     public static void AddUnionMember(int memberId, Employee employee)
     {
-        if (s_employees.ContainsKey(employee.ID) is false)
+        if (s_Employees.ContainsKey(employee.ID) is false)
             throw new InvalidOperationException("Employee does not exist");
-        s_affiliations.Add(memberId, employee);
+        s_Affiliations.Add(memberId, employee);
     }
 
     public static Employee? GetUnionMember(int memberId)
-        => s_affiliations.GetValueOrDefault(memberId);
+        => s_Affiliations.GetValueOrDefault(memberId);
 
     public static void RemoveUnionMember(int memberId)
-        => s_affiliations.Remove(memberId);
+        => s_Affiliations.Remove(memberId);
 }
--- test/Payroll.Test/DeleteTransactionTest.cs
+++ test/Payroll.Test/DeleteTransactionTest.cs
@@ -1,8 +1,7 @@
-using Payroll.Application.Transactions.Add;
 using Payroll.Application.Transactions.Delete;
 
 using Payroll.Core.Data;
-using Payroll.Core.Entities;
+using TestUtilities.Mocks.Employee;
 
 namespace Payroll.Test;
 
@@ -12,17 +11,13 @@ public class DeleteTransactionTest
     [Test]
     public void Test_DeleteEmployee()
     {
-        int id = 4;
-        AddCommissionedEmployee t = new(id, "Terry", "Home", 2500, 3.2);
+        var (t, expected) = EmployeeMockFactory.CreateCommissionedMock();
+        var dt = new DeleteEmployeeTransaction(expected.Id);
+        
         t.Execute();
-
-        Employee? e = PayrollDb.GetEmployee(id);
-        Assert.That(e, Is.Not.Null);
-
-        DeleteEmployeeTransaction dt = new(id);
         dt.Execute();
-
-        e = PayrollDb.GetEmployee(id);
+        var e = PayrollDb.GetEmployee(expected.Id);
+        
         Assert.That(e, Is.Null);
     }
 }
