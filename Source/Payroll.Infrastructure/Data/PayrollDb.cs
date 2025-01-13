f61fd57ad594664aa0122c431ee451d8b10fe614
csvitor-dev <csvitor.dev@gmail.com>
2025-01-13 08:24:40 -0300

    chore: add new project that represents infrastructure layer

--- Source/Payroll.Infrastructure/Data/PayrollDb.cs
+++ Source/Payroll.Infrastructure/Data/PayrollDb.cs
@@ -1,36 +1,36 @@
 using Payroll.Core.Entities;
 
-namespace Payroll.Core.Data;
+namespace Payroll.Infrastructure.Data;
 
 public static class PayrollDb
 {
-    private static readonly Dictionary<int, Employee> s_Employees = [];
-    private static readonly Dictionary<int, Employee> s_Affiliations = [];
+    private static readonly Dictionary<int, Employee> Employees = [];
+    private static readonly Dictionary<int, Employee> Affiliations = [];
 
     // Employee Table
     public static void AddEmployee(int id, Employee employee)
-        => s_Employees.Add(id, employee);
+        => Employees.Add(id, employee);
 
     public static Employee? GetEmployee(int empId)
-        => s_Employees.GetValueOrDefault(empId);
+        => Employees.GetValueOrDefault(empId);
     
     public static IList<Employee> GetEmployees()
-        => s_Employees.Values.ToList();
+        => Employees.Values.ToList();
 
     public static void DeleteEmployee(int empId)
-        => s_Employees.Remove(empId);
+        => Employees.Remove(empId);
 
     // Affiliation Table
     public static void AddUnionMember(int memberId, Employee employee)
     {
-        if (s_Employees.ContainsKey(employee.ID) is false)
+        if (Employees.ContainsKey(employee.ID) is false)
             throw new InvalidOperationException("Employee does not exist");
-        s_Affiliations.Add(memberId, employee);
+        Affiliations.Add(memberId, employee);
     }
 
     public static Employee? GetUnionMember(int memberId)
-        => s_Affiliations.GetValueOrDefault(memberId);
+        => Affiliations.GetValueOrDefault(memberId);
 
     public static void RemoveUnionMember(int memberId)
-        => s_Affiliations.Remove(memberId);
+        => Affiliations.Remove(memberId);
 }
